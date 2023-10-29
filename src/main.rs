bbf4e7b0c170a86077431483e5a983947ef65b19
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-10-29 16:59:06 +0530

    var and mut

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::io;
-
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your number");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed to read line");
-    println!("You guessed : {guess}"); 
-    // can also be written as
-    println!("You guessed : {}",guess);
+   let x = 15;
+   println!("value of x is {x}");
+    //    x = 10 
+    // cannot update the value of x we need to make it mutable like this
+    let mut y = 20;
+    println!("value of y is {y}");
+    y = 11;
+    println!("new value of y is {y}");
 }
